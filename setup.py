759f06e6eadc2abe27c0a494f597a1e2af256511
Alexis Bellido <alexis@ventanazul.com>
2019-07-04 12:57:37 -0400

    update settings docs

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         'Framework :: Django',
         'Framework :: Django :: 2.0',
         'Framework :: Django :: 2.1',
+        'Framework :: Django :: 2.2',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
